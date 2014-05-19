6f13564daa60950529eb5e8b9f974709a0ddfab7
André König <andre.koenig@gmail.com>
2014-05-19 09:20:21 +0200

    Moved lines.

--- advanced.js
+++ advanced.js
@@ -4,9 +4,9 @@ var platform = require('platform');
 
 var ua = 'Mozilla/5.0 (X11; Linux i686; rv:30.0) Gecko/20100101 Firefox/30.0';
 
-console.log('Result from parsing the user agent string: %s \n', ua);
-
 var browser = platform.parse(ua);
+
+console.log('Result from parsing the user agent string: %s \n', ua);
 
 console.log(browser.name); // Firefox
 console.log(browser.description); // Firefox 30.0 on Linux i686
